c372e9ad4346ced9102150ca15b73bb637b14527
Archan Patkar <pat@archanpatkar.com>
2020-10-14 19:46:39 +0530

    refac

--- src/avaran.js
+++ src/avaran.js
@@ -1,6 +1,12 @@
 // Add Fantasy Land and Static Land support.
 const { sum, tagged } = require("./styp");
 
+// Free Monad
+const Free = sum("Free", {
+    Return:["v"],
+    Suspend:["f"]
+});
+
 // Either Monad
 // Right biased
 const Either = sum("Either", {
@@ -48,7 +54,7 @@ Either.prototype.bimap = function(rTransformer,lTransformer) {
         Right: ({ v }) => Either.Right(rTransformer(v)),
         Left: ({ v }) => Either.Left(lTransformer(v))
     });
-}
+};
 
 Either.prototype.swap = function() {
     return this.cata({
@@ -84,21 +90,21 @@ Either.prototype.right = function() {
         Right: ({ v }) => v,
         Left: () => { throw new Error("Either.Right was expected") }
     });
-}
+};
 
 Either.prototype.left = function() {
     return this.cata({
         Right: () => { throw new Error("Either.Left was expected") },
         Left: ({ v }) => v
     });
-}
+};
 
 Either.prototype.contains = function(val) {
     return this.cata({
         Right: ({ v }) => v == val,
         Left: ({ v }) => v == val
     });
-}
+};
 
 // Reader Monad
 const Reader = tagged("Reader", ["action"]);
@@ -178,7 +184,7 @@ Maybe.prototype.foldLeft = function(def, transformer) {
     return this.cata({
         Some: ({ v }) => transformer(def,v),
         None: () => def
-    })
+    });
 };
 
 Maybe.prototype.isSome = function() {
@@ -193,21 +199,21 @@ Maybe.prototype.some = function() {
     return this.cata({
         Some: ({ v }) => v,
         None: () => { throw new Error("Maybe.some: No value present"); }
-    })
-}
+    });
+};
 
 Maybe.prototype.orSome = function(def) {
     return this.cata({
         Some: ({ v }) => v,
         None: () => def
-    })
+    });
 };
 
 Maybe.prototype.orLazy = function(def) {
     return this.cata({
         Some: ({ v }) => v,
         None: () => def()
-    })
+    });
 };
 
 Maybe.prototype.orNull = function() {
@@ -222,14 +228,14 @@ Maybe.prototype.orUndefined = function() {
         Some: ({ v }) => v,
         None: () => undefined
     });
-}
+};
 
 Maybe.prototype.orElse = function(other) {
     return this.cata({
         Some: () => this,
         None: () => other
     });
-}
+};
 
 Maybe.prototype.orNoneif = function(bool) {
     return bool? Maybe.None: this;
@@ -239,7 +245,7 @@ Maybe.prototype.do = function(fn) {
     this.cata({
         Some: ({ v }) => fn(v),
         None: () => {}
-    })
+    });
 };
 
 Maybe.prototype.orElseRun = function(fn) {
@@ -247,32 +253,42 @@ Maybe.prototype.orElseRun = function(fn) {
         Some: () => {},
         None: () => fn()
     });
-}
+};
 
 Maybe.prototype.filter = function(cond) {
     return this.cata({
         Some: ({ v }) => cond(v)? this: Maybe.None,
         None: () => this
     });
-}
+};
 
 Maybe.prototype.toArray = function() {
     return this.cata({
         Some: ({ v }) => [v],
         None: () => []
     });
-}
+};
 
 Maybe.prototype.toBool = function() {
     return this.cata({
         Some: () => true,
         None: () => false
     });
-}
+};
 
 Maybe.prototype.cataM = function(ifSome, ifNone) {
     return this.cata({
         Some: ({ v }) => ifSome(v),
         None: ifNone
     });
-}
+};
+
+const avaran = Object.freeze({
+    Maybe: Maybe,
+    Either: Either,
+    IO:IO,
+    Reader:Reader,
+    Free:Free
+});
+
+if(typeof module != "undefined") module.exports = avaran;
