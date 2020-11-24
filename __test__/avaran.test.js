5054d79099deb24c3f3db7ccaeea9bbbee8c934c
Archan Patkar <pat@archanpatkar.com>
2020-11-24 13:51:52 +0530

    placeholder

---
+++ __test__/avaran.test.js
@@ -0,0 +1,22 @@
+const avaran = require("../src/avaran");
+
+test('avaran.Maybe', () => {
+    // const Point3D = tagged("Point3D", ["x","y","z"])
+    // const a = Point3D(1, 2, 3)
+    // expect(Point3D.toString()).toEqual("Point3D")
+    // expect(a).toEqual({ x:1, y:2, z:3 })
+    // expect(a.x == 1 && a.y == 2 && a.z == 3).toEqual(true)
+    // expect(a.toString()).toEqual("Point3D(1,2,3)")
+    // expect(Point3D.is(a)).toEqual(true)
+    // Point3D.prototype.scale = function(n){
+    //     return Point3D(this.x * n, this.y * n, this.z * n)
+    // }
+    // const b = a.scale(2)
+    // expect(b.toString()).toEqual("Point3D(2,4,6)")
+    // const c = Point3D.from({y: 2, x: 1, z: 3})
+    // expect(c).toEqual({ x: 1, y: 2, z: 3 })
+    // expect(c.toString()).toEqual("Point3D(1,2,3)")
+    // expect(() => {
+    //     Point3D(1,2)
+    // }).toThrow(TypeError);
+})
